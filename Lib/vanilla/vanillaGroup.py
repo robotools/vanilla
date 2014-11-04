99fa2615b8e7bd8d0aec6161a09204dd87e302ea
Tal Leming <tal@Tals-MacBook-Air.local>
2014-11-04 10:13:09 -0500

    Support new visual effect views.

--- Lib/vanilla/vanillaGroup.py
+++ Lib/vanilla/vanillaGroup.py
@@ -1,5 +1,29 @@
 from AppKit import NSView
-from vanillaBase import VanillaBaseObject
+from vanillaBase import VanillaBaseObject, osVersion
+
+try:
+    NSVisualEffectMaterialAppearanceBased
+    NSVisualEffectMaterialLight
+    NSVisualEffectMaterialDark
+    NSVisualEffectMaterialTitlebar
+    NSVisualEffectBlendingModeBehindWindow
+    NSVisualEffectBlendingModeWithinWindow
+except NameError:
+    NSVisualEffectMaterialAppearanceBased = 0
+    NSVisualEffectMaterialLight = 1
+    NSVisualEffectMaterialDark = 2
+    NSVisualEffectMaterialTitlebar = 3
+    NSVisualEffectBlendingModeBehindWindow = 0
+    NSVisualEffectBlendingModeWithinWindow = 1
+
+if osVersion >= "10.10":
+    from AppKit import NSVisualEffectView, CALayer
+    _blendingModeMap = {
+        "behindWindow" : NSVisualEffectBlendingModeBehindWindow,
+        "withinWindow" : NSVisualEffectBlendingModeWithinWindow
+    }
+else:
+    NSVisualEffectView = None
 
 
 class Group(VanillaBaseObject):
@@ -25,15 +49,72 @@ class Group(VanillaBaseObject):
     No special naming is required for the attributes. However, each attribute must have a unique name.
 
     **posSize** Tuple of form *(left, top, width, height)* representing the position and size of the group.
+
+    **blendingMode** The blending mode for the window. These are the possible options:
+
+    +----------------+-------------------------------------------+
+    | None           | No special blending.                      |
+    +----------------+-------------------------------------------+
+    | "behindWindow" | Blend with the content behind the window. |
+    +----------------+-------------------------------------------+
+    | "withinWindow" | Blend with the content within the window. |
+    +----------------+-------------------------------------------+
     """
 
     nsViewClass = NSView
+    nsVisualEffectViewClass = NSVisualEffectView
 
-    def __init__(self, posSize):
+    def __init__(self, posSize, blendingMode=None):
         self._setupView(self.nsViewClass, posSize)
+        if blendingMode is not None and osVersion >= "10.10":
+            # the visual effect view is defined as a subview
+            # of the basic NSView. this is necessary because
+            # within window blending requires a CA layer and
+            # we may as well do it for behind window blending
+            # as well to make the code in here clear.
+            self._visualEffectGroup = _VisualEffectGroup(self.nsVisualEffectViewClass)
+            if blendingMode == "withinWindow":
+                self._visualAppearanceLayer = CALayer.layer()
+                self._nsObject.setLayer_(self._visualAppearanceLayer)
+                self._nsObject.setWantsLayer_(True)
+            blendingMode = _blendingModeMap[blendingMode]
+            self._visualEffectGroup.getNSVisualEffectView().setBlendingMode_(blendingMode)
+
+    def __setattr__(self, attr, value):
+        # __init__
+        if not hasattr(self, "_visualEffectGroup") or attr == "_visualEffectGroup":
+            super(Group, self).__setattr__(attr, value)
+        # adding a vanilla subview: add to the visual effect view
+        elif hasattr(self, "_visualEffectGroup") and isinstance(value, VanillaBaseObject) and hasattr(value, "_posSize"):
+            value._nsObject.setAllowsVibrancy_(True)
+            print value._nsObject.effectiveAppearance()
+            setattr(self._visualEffectGroup, attr, value)
+        # fallback: add to the main view
+        else:
+            super(Group, self).__setattr__(attr, value)
+
+    def __delattr__(self, attr):
+        if hasattr(self, attr):
+            super(Group, self).__delattr__(attr)
+        elif hasattr(self, "_visualEffectView"):
+            delattr(self._visualEffectView, attr)
 
     def getNSView(self):
         """
         Return the *NSView* that this object wraps.
         """
+        return self._nsObject
+
+    def getNSVisualEffectView(self):
+        """
+        Return the *NSVisualEffectView* that this object wraps.
+        """
+
+
+class _VisualEffectGroup(VanillaBaseObject):
+
+    def __init__(self, cls):
+        self._setupView(cls, (0, 0, 0, 0))
+
+    def getNSVisualEffectView(self):
         return self._nsObject
