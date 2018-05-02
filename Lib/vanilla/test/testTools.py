948f6ae6c6a9dacb6d2282ff5e129b231abb8903
Frederik Berlaen <frederik@typemytype.com>
2018-05-02 10:27:59 +0200

    import all required classes

--- Lib/vanilla/test/testTools.py
+++ Lib/vanilla/test/testTools.py
@@ -1,5 +1,5 @@
 from Foundation import NSObject
-from AppKit import NSApplication, NSMenu, NSMenuItem
+from AppKit import NSApplication, NSMenu, NSMenuItem, NSBundle
 from PyObjCTools import AppHelper
 
 
