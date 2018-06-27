04f4ed09c504d913ed65b6992d2dd67866fd61ba
Frederik Berlaen <frederik@typemytype.com>
2018-06-27 17:50:22 +0200

    add support for images, states and enableing menu items

--- Lib/vanilla/vanillaList.py
+++ Lib/vanilla/vanillaList.py
@@ -195,6 +195,17 @@ class _VanillaArrayController(VanillaArrayController):
 
 
 def VanillaMenuBuilder(sender, items, menu):
+    """
+    Build a menu from a given set of items
+    Each items must be a dict with the following keys:
+
+    * **title** title of the menu item (required)
+    * **callback** callback when the menu item is clicked (optional)
+    * **items** a list of sub menu items, this will build a sub menu for the given menu item.
+    * **image** an image placed inside the menu item, must be a NSImage.
+    * **state** a menu item state: must be either 0, 1 or -1 (on, off or mixed).
+    * **enabled** enable the menu item, must be a bool.
+    """
     sender._menuItemCallbackWrappers = []
     for item in items:
         if isinstance(item, NSMenuItem):
@@ -206,6 +217,9 @@ def VanillaMenuBuilder(sender, items, menu):
             title = item["title"]
             callback = item.get("callback")
             subItems = item.get("items")
+            image = item.get("image")
+            state = item.get("state")
+            enabled = item.get("enabled")
 
             menuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(title, "", "")
             if callback:
@@ -217,6 +231,14 @@ def VanillaMenuBuilder(sender, items, menu):
                 subMenu = NSMenu.alloc().init()
                 VanillaMenuBuilder(subItems, subMenu)
                 menuItem.setSubmenu_(subMenu)
+
+            if image is not None:
+                menuItem.setImage_(image)
+            if state is not None:
+                menuItem.setState_(state)
+            if enabled is not None:
+                menuItem.setEnabled_(enabled)
+
             menu.addItem_(menuItem)
 
 
