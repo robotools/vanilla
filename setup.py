dc09ff3b21e91d9e63d6cff5bfc146b5ccf28a75
justvanrossum <justvanrossum@gmail.com>
2021-04-15 21:10:24 +0200

    make html gives many errors; need to fix later if it is still desired to work like this

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
-import sys
+# import sys
 from setuptools import setup
 
-if "sdist" in sys.argv:
-    import os
-    import subprocess
-    import shutil
-    docFolder = os.path.join(os.getcwd(), "documentation")
-    # remove existing
-    doctrees = os.path.join(docFolder, "build", "doctrees")
-    if os.path.exists(doctrees):
-        shutil.rmtree(doctrees)
-    # compile
-    p = subprocess.Popen(["make", "html"], cwd=docFolder)
-    p.wait()
-    # remove doctrees
-    shutil.rmtree(doctrees)
 
+# if "sdist" in sys.argv:
+#     import os
+#     import subprocess
+#     import shutil
+#     docFolder = os.path.join(os.getcwd(), "Documentation")
+#     # remove existing
+#     doctrees = os.path.join(docFolder, "build", "doctrees")
+#     if os.path.exists(doctrees):
+#         shutil.rmtree(doctrees)
+#     # compile
+#     p = subprocess.Popen(["make", "html"], cwd=docFolder)
+#     p.wait()
+#     # remove doctrees
+#     shutil.rmtree(doctrees)
 
 
 setup(name="vanilla",
